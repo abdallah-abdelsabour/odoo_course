3f5e12b87643d3069fe26e661bf0aef89be7f3cf
Abdallah <Abdallah97158@gmail.com>
2023-03-17 21:03:31 +0200

    first-commit

--- custome/hms/__init__.py
+++ custome/hms/__init__.py
@@ -1,3 +1,1 @@
 from . import models
-from . import departments   
-from . import doctor
--- custome/hms/__manifest__.py
+++ custome/hms/__manifest__.py
@@ -3,7 +3,7 @@
     'name':"hms_odoo",
     'version':'1.0',
     'data' : [
-    'views/createpatients.xml',
+    'views/patient.xml',
       'views/departments.xml',
         'views/doctor.xml',
     ]
